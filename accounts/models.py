6143dd48adfdaff9b645ec8f408a924fae87b5bc
carojas <carojasb94@gmail.com>
2025-04-19 13:57:56 -0600

    [CA4] Add indexes

--- accounts/models.py
+++ accounts/models.py
@@ -9,6 +9,11 @@ class CollectionAgency(models.Model):
 
     name = models.CharField(max_length=255)
 
+    class Meta:
+        indexes = [
+            models.Index(fields=["name"]),
+        ]
+
     def __str__(self):
         return self.name
 
@@ -19,6 +24,11 @@ class Client(models.Model):
     name = models.CharField(max_length=255)
     agency = models.ForeignKey(CollectionAgency, on_delete=models.CASCADE, related_name="clients")
     reference_no = models.CharField(max_length=64, unique=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["agency"]),
+        ]
 
     def __str__(self):
         return self.name
@@ -31,6 +41,11 @@ class Consumer(models.Model):
     address = models.TextField()
     ssn = models.CharField(max_length=11, unique=False)  # e.g., '123-45-6789'
     is_entity = models.BooleanField(default=False)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["name"]),  # required to optimize the search by name
+        ]
 
     def __str__(self):
         return self.name
@@ -46,6 +61,14 @@ class Debt(models.Model):
 
     client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name="debts")
     consumers = models.ManyToManyField(Consumer, related_name="debts")
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["balance"]),
+            models.Index(fields=["status"]),
+            models.Index(fields=["client_reference_no"]),
+            models.Index(fields=["created_at"]),
+        ]
 
     def clean(self):
         """Validates that the balance is greater than zero."""
